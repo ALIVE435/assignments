fd599e33a760234c747d2b34aa5b9d4745421b24
ALIVE435 <aniketksingh01@gmail.com>
2023-12-21 23:19:08 +0530

    fileServer solution added

--- week-2/02-nodejs/fileServer.solution.js
+++ week-2/02-nodejs/fileServer.solution.js
@@ -14,8 +14,33 @@
  */
     const express = require('express');
     const fs = require('fs');
-    const path = require('path');
+    ///const path = require('path');
     const app = express();
     
+    app.get("/files",(req,res)=>{
+      fs.readdir(__dirname+"/files",(err,files)=>{
+        if(err){
+          return res.status(500).json({ error: 'Failed to retrieve files' });
+        }
+        else{
+          res.status(200).json(files);
+        }
+      })
+    })
     
+    app.get("/file/:filename",(req,res)=>{
+      const filename = req.params.filename;
+      fs.readFile(__dirname+"/files/"+filename,"utf-8",(err,data)=>{
+        if (err) {
+          return res.status(404).send('File not found');
+        }
+        res.status(200).send(data);
+      })
+    })
+    
+    app.all('*', (req, res) => {
+      res.status(404).send('Route not found');
+    });
+    
+    //app.listen(3500);
     module.exports = app;
